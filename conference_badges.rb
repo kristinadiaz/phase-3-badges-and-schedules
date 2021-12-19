210a2a3ec31e27e6de5e6ec9eb8e1d2c8d55d978
Kristina Diaz <kristina.diaz84@gmail.com>
2021-12-18 19:13:55 -0500

    commit tree fde6f58fbf617ed44922a9fcfc9b136471d5e173

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker name
+    p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |n| "Hello, my name is #{n}."
+    end
+end
+
+def assign_rooms array
+    array.map.with_index(1) do |n, rm|
+        "Hello, #{n}! You'll be assigned to room #{rm}!"
+    end
+end
+
+def printer array
+  batch_badge_creator(array).each do |b|
+    puts b
+  end
+  assign_rooms(array).each do |n|
+    puts n
+  end
+end
